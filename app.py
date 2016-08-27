ca058e1a098a0069bba69f63786c78a088381627
helelily <hsajer@gmail.com>
2016-08-27 15:58:15 -0400

    link page test

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+app.vars={}
+
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
-  	
+	if request.method == 'POST':
+        app.vars['ticker'] = request.form['ticker']
+        return render_template('graph.html', ticker=app.vars['ticker'])
+    else:
+        return render_template('index.html')
+
+ @app.route('/graph')
+ def graph():
+ 	return render_template('graph.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
