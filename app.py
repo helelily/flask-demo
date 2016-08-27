93579b4fa4a5c2e649aa5d1025aec1a790ba5e5a
helelily <hsajer@gmail.com>
2016-08-27 15:03:25 -0400

    try fixing again

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def main():
-#  return redirect('/index')
+@app.route('/')
+def main():
+  return redirect('/index')
 
-@app.route('/index', methods=['GET'])
+@app.route('/index')
 def index():
     if request.method == 'GET'
   		return render_template('index.html')
