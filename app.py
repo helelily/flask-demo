1af6f59b7dd3646828abc94c0497012e4e91d8d4
helelily <hsajer@gmail.com>
2016-08-27 15:00:16 -0400

    fix error

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
+#@app.route('/')
+#def main():
+#  return redirect('/index')
 
 @app.route('/index', methods=['GET'])
 def index():
@@ -14,4 +14,4 @@ def index():
   		return 'request.method was not a GET!'
 
 if __name__ == '__main__':
-  app.run(port=33507, debug=True)
+  app.run(port=33507)
