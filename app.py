9899771856fdeebcecc409e588e9ceadee43bcae
helelily <hsajer@gmail.com>
2016-08-27 16:00:51 -0400

    back to basics

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-app.vars={}
-
 @app.route('/')
 def main():
   return redirect('/index')
@@ -11,14 +9,9 @@ def main():
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-        app.vars['ticker'] = request.form['ticker']
-        return render_template('graph.html', ticker=app.vars['ticker'])
+        return render_template('index.html')
     else:
         return render_template('index.html')
-
- @app.route('/graph')
- def graph():
- 	return render_template('graph.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
